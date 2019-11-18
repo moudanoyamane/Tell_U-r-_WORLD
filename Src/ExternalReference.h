893dc4806dbc6a92f85e57842358fd3699210e10
moudanoyamane <moudanoyamane@gmail.com>
2019-11-18 15:56:41 +0900

    ビルド不能だったので修正

--- Src/ExternalReference.h
+++ Src/ExternalReference.h
@@ -8,7 +8,7 @@
 
 #include "../Src/stdafx.h"
 
-#include "..//..//wavefile_io/Src/wavefile_io.h"
-#include "..//..//eight_c_and_c/Src/eight_c_and_c.h"
+#include "../../wavefile_io/Src/wavefile_io.h"
+#include "../../eight_c_and_c/Src/eight_c_and_c.h"
 #include "../../jewelwasp/Src/JewelWasp.h"
 #include "../../jewelwasp/Test/utilizeWORLD.h"
